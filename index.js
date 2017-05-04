683e2c4167b9d717c13e2fdb1feebf2072255d4c
CWD <cwd@cloudspokes.com>
2017-05-04 15:03:24 -0400

    fetching groups and members

--- index.js
+++ index.js
@@ -6,25 +6,32 @@ const logger = require('winston');
 const requestPromise = require('request-promise');
 const LinkHeader = require('http-link-header');
 const inquirer = require('inquirer');
+const Rx = require('rx');
 
 const ui = new inquirer.ui.BottomBar();
 
 const version = 3;
-const perPage = 100;
+const perPage = 40;
 const urlBase = `https://gitlab.com/api/v${version}`;
-// /694205/members \.'
+
+const HTTP_200 = 200;
+const ACCESS_GUEST = 10;
+const ACCESS_REPORTER = 20;
+const ACCESS_DEVELOPER = 30;
+const ACCESS_MASTER = 40;
+const ACCESS_OWNER = 50;
 
 /**
- * callOut - description
+ * fetchData - description
  *
  * @param  {type} url description
- * @param  {type} headers description
+ * @param  {type} payload description
  * @param  {type} cb  description
  */
-function callOut(url, headers, cb) {
-  if (_.isFunction(headers)) {
-    cb = headers;
-    headers = {};
+function fetchData(url, payload, cb) {
+  if (_.isFunction(payload)) {
+    cb = payload;
+    payload = [];
   }
 
   cb = _.isFunction(cb) ? cb : (err) => {
@@ -33,20 +40,33 @@ function callOut(url, headers, cb) {
     }
   };
 
-  headers = _.merge({'PRIVATE-TOKEN': config.PRIVATE_TOKEN}, headers || {});
-
   const obj = {
     resolveWithFullResponse: true,
     simple: false,
     json: true,
-    headers,
+    headers: {'PRIVATE-TOKEN': config.PRIVATE_TOKEN},
     uri: `${url}`
   };
 
   logger.log('info', `Calling out to ${obj.uri}`);
   const p = requestPromise(obj);
   p.then((resp) => {
-    cb(null, resp);
+    if (resp.statusCode !== HTTP_200) {
+      logger.log('error', resp.body);
+      return cb(resp.body); // CWD-- bail out with an error
+    }
+
+    payload = _.concat(payload, resp.body);
+    url = getNextURL(resp.headers);
+    logger.log('debug', `next page: ${_.get(resp.headers, 'X-Next-Page', '')}`);
+
+    if (url) {
+      logger.log('info', 'calling for more payload');
+      return fetchData(url, payload, cb);
+    }
+
+    logger.log('info', `returning ${payload.length} records`);
+    return cb(null, payload);
   })
   .catch((err) => {
     cb(err);
@@ -55,49 +75,57 @@ function callOut(url, headers, cb) {
 
 
 /**
- * getGroups - description
+ * getNextURL - description
  *
- * @param  {type} url description
- * @param  {type} groups description
- * @param  {type} cb callback
+ * @param  {type} headers description
+ * @returns {type}         description
  */
-function getGroups(url, groups, cb) {
-  if (_.isFunction(url)) { // CWD-- if we got just the cb then default the args
-    cb = url;
-    url = null;
-    groups = null;
-  }
+function getNextURL(headers) {
+  const link = LinkHeader.parse(_.get(headers, 'link', ''));
+  const aNext = link.get('rel', 'next');
 
+  return aNext.length > 0 ? aNext[0].uri : null;
+}
+
+
+function removeMember(groupId, memberId, cb) {
+
+}
+
+/**
+ * getMembersForGroup - description
+ *
+ * @param  {type} groupId description
+ * @param  {type} cb      description
+ */
+function getMembersForGroup(groupId, cb) {
   cb = _.isFunction(cb) ? cb : (err) => { // CWD-- make sure we've got a function for a cb
     if (err) {
       logger.log('debug', err);
     }
   };
 
-  url = url || `${urlBase}/groups?per_page=${perPage}`; // CWD-- set the starting url if we're not iterating yet
-  groups = groups || [];
+  const url = `${urlBase}/groups/${groupId}/members?access_level=${ACCESS_DEVELOPER}&per_page=${perPage}`; // CWD-- set the starting url if we're not iterating yet
 
-  callOut(url, (err, resp) => {
+  fetchData(url, cb);
+}
+
+/**
+ * getGroups - description
+ *
+ * @param  {type} url description
+ * @param  {type} groups description
+ * @param  {type} cb callback
+ */
+function getGroups(cb) {
+  cb = _.isFunction(cb) ? cb : (err) => { // CWD-- make sure we've got a function for a cb
     if (err) {
       logger.log('debug', err);
-      return cb(err); // CWD-- bail out with an error
     }
+  };
 
-    groups = _.concat(groups, resp.body);
-    const link = LinkHeader.parse(_.get(resp.headers, 'link', ''));
-    const aNext = link.get('rel', 'next');
-
-    url = aNext.length > 0 ? aNext[0].uri : null;
-    logger.log('debug', `next page: ${_.get(resp.headers, 'X-Next-Page', '')}`);
-
-    if (url) {
-      logger.log('info', 'calling for more groups');
-      return getGroups(url, groups, cb);
-    }
-
-    logger.log('info', `returning ${groups.length} groups`);
-    return cb(null, groups);
-  });
+  const url = `${urlBase}/groups?per_page=${perPage}`; // CWD-- set the starting url if we're not iterating yet
+  fetchData(url, cb);
 }
 
 inquirer.prompt([{
@@ -118,24 +146,44 @@ inquirer.prompt([{
   if ((startAnswer.execute === 'yes') && startAnswer.privateToken) {
     config.PRIVATE_TOKEN = config.PRIVATE_TOKEN || startAnswer.privateToken;
 
-    return getGroups((err, data) => {
+    return getGroups((getGroupsErr, groupData) => {
       logger.log('debug', 'callback returned');
-      if (err) {
-        logger.log('debug', err);
+      if (getGroupsErr) {
+        logger.log('debug', getGroupsErr);
       } else {
-        console.log(data);
-        _.forEach(data, (v, k) => {
-          inquirer.prompt([{
-            type: 'list',
-            name: 'execute',
-            message: `clean out '${v.name}'?`,
-            default: 'yes',
-            choices: ['yes', 'no']
-          }]).then((groupAnswer) => {
-            if (groupAnswer.execute === 'yes') {
-              logger.log('debug', k, v);
-            } else {
-              logger.log('debug', `ok. skipping grou '${v.name}'`);
+        const observe = Rx.Observable.create((obs) => {
+          _.forEach(groupData, (v, k) => {
+            obs.onNext({
+              type: 'list',
+              name: `execute_${k}`,
+              message: `${k}: clean out '${v.name}'?`,
+              default: {name: 'yes', value: v.id},
+              choices: [{name: 'yes', value: v.id}, {name: 'no', value: null}],
+              validate: (answer) => {
+                return answer;
+              }
+            });
+          });
+          obs.onCompleted();
+        });
+
+        inquirer.prompt(observe).then((answers) => {
+          _.forEach(answers, (groupId) => {
+            if (groupId) {
+              getMembersForGroup(groupId, (getMembersErr, members) => {
+                console.log(getMembersErr, members);
+                if (getMembersErr) {
+                  logger.log('error', getMembersErr);
+                } else {
+                  logger.log('info', `removing members for group: ${groupId}`);
+                  _.forEach(members, (member, iM) => {
+                    logger.log('info', `removing member: ${member.username} from group ${groupId}`);
+                    removeMember(groupId, member.id, (removeMemberErr, data) => {
+                      console.log(removeMemberErr, data);
+                    });
+                  });
+                }
+              });
             }
           });
         });
